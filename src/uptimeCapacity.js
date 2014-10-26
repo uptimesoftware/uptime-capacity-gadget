6e21c29f9f73bf975831c204fe02ac6058022c28
dan branton <daniel.branton@uptimesoftware.com>
2014-10-26 16:29:54 -0400

    initial framework for the gadget

---
+++ src/uptimeCapacity.js
@@ -0,0 +1,197 @@
+$(function() {
+	var api = new apiQueries();
+	var myChart = null;
+	var myChartDimensions = null;
+	var uptimeCapacitySettings = {
+		entityId : -1,
+		refreshInterval : 30,
+		chartTypeId : "pie",
+	};
+	var divsToDim = [ '#widgetChart', '#widgetSettings' ];
+
+	$("#widgetSettings").hide();
+
+	$('.element-status-setting').change(settingChanged);
+
+	$("#closeSettings").click(function() {
+		$("#widgetSettings").slideUp();
+	});
+
+	uptimeGadget.registerOnEditHandler(showEditPanel);
+	uptimeGadget.registerOnLoadHandler(function(onLoadData) {
+		myChartDimensions = toMyChartDimensions(onLoadData.dimensions);
+		if (onLoadData.hasPreloadedSettings()) {
+			goodLoad(onLoadData.settings);
+		} else {
+			uptimeGadget.loadSettings().then(goodLoad, onBadAjax);
+		}
+	});
+	uptimeGadget.registerOnResizeHandler(resizeGadget);
+
+	escapeHtml = function(str) {
+		return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	};
+
+	monitorCount = function(count) {
+		if (count == 0) {
+			return "No monitors";
+		} else if (count == 1) {
+			return "1 monitor";
+		}
+		return count + " monitors";
+	};
+
+	function resizeGadget(dimensions) {
+		myChartDimensions = toMyChartDimensions(dimensions);
+		if (myChart) {
+			myChart.resize(myChartDimensions);
+		}
+		$("body").height($(window).height());
+	}
+
+	function toMyChartDimensions(dimensions) {
+		return new UPTIME.pub.gadgets.Dimensions(Math.max(100, dimensions.width - 5), Math.max(100, dimensions.height - 5));
+	}
+
+	function settingChanged() {
+		uptimeCapacitySettings.chartTypeId = $("#widgetOptions input[name=chartType]:radio:checked").val();
+		uptimeCapacitySettings.elementId = $('#elementId').find(":selected").val();
+		uptimeCapacitySettings.elementName = $('#elementId').find(":selected").text();
+		uptimeCapacitySettings.refreshInterval = $("#refreshRate").val();
+		uptimeGadget.saveSettings(uptimeCapacitySettings).then(onGoodSave, onBadAjax);
+	}
+
+	function displayStatusBar(error, msg) {
+		gadgetDimOn();
+		var statusBar = $("#statusBar");
+		statusBar.empty();
+		var errorBox = uptimeErrorFormatter.getErrorBox(error, msg);
+		errorBox.appendTo(statusBar);
+		statusBar.slideDown();
+	}
+
+	function clearStatusBar() {
+		gadgetDimOff();
+		var statusBar = $("#statusBar");
+		statusBar.slideUp().empty();
+	}
+
+	function showEditPanel() {
+		if (myChart) {
+			myChart.stopTimer();
+		}
+
+		$("#widgetOptions input[name=chartType]").filter('[value=' + uptimeCapacitySettings.chartTypeId + ']').prop('checked',
+				true);
+		$('#elementId').val(uptimeCapacitySettings.elementId);
+		$("#refreshRate").val(uptimeCapacitySettings.refreshInterval);
+
+		$("#widgetSettings").slideDown();
+		$("body").height($(window).height());
+		return populateIdSelector().then(function() {
+			if (!myChart) {
+				settingChanged();
+			}
+		}, function(error) {
+			displayStatusBar(error, "Error Loading the List of Elements from up.time Controller");
+		});
+	}
+
+	function disableSettings() {
+		$('.element-status-setting').prop('disabled', true);
+		$('#closeButton').prop('disabled', true).addClass("ui-state-disabled");
+	}
+
+	function enableSettings() {
+		$('.element-status-setting').prop('disabled', false);
+		$('#closeButton').prop('disabled', false).removeClass("ui-state-disabled");
+	}
+
+	function elementSort(arg1, arg2) {
+		return naturalSort(arg1.name, arg2.name);
+	}
+
+	function populateIdSelector() {
+		disableSettings();
+		$('#elementId').empty().append($("<option />").val(-1).text("Loading..."));
+		return api.getAllElements().then(function(elements) {
+			clearStatusBar();
+			enableSettings();
+			// fill in element drop down list
+			elements.sort(elementSort);
+			var elementSelector = $('#elementId').empty();
+			$.each(elements, function() {
+				if (!this.isMonitored) {
+					return;
+				}
+				elementSelector.append($("<option />").val(this.id).text(this.name));
+			});
+			if (uptimeCapacitySettings.elementId >= 0) {
+				elementSelector.val(uptimeCapacitySettings.elementId);
+			}
+		});
+	}
+
+	function goodLoad(settings) {
+		clearStatusBar();
+		if (settings) {
+			$("#elementId").val(settings.elementId);
+			$("#" + settings.chartTypeId).prop("checked", true);
+			$("#refreshRate").val(settings.refreshInterval);
+			$.extend(uptimeCapacitySettings, settings);
+			displayChart();
+		} else if (uptimeGadget.isOwner()) {
+			$('#widgetChart').hide();
+			showEditPanel();
+		}
+	}
+
+	function onGoodSave() {
+		clearStatusBar();
+		displayChart();
+	}
+
+	function onBadAjax(error) {
+		displayStatusBar(error, "Error Communicating with up.time");
+	}
+
+	function gadgetDimOn() {
+		$.each(divsToDim, function(i, d) {
+			var div = $(d);
+			if (div.is(':visible') && div.css('opacity') > 0.6) {
+				div.fadeTo('slow', 0.3);
+			}
+		});
+	}
+
+	function gadgetDimOff() {
+		$.each(divsToDim, function(i, d) {
+			var div = $(d);
+			if (div.is(':visible') && div.css('opacity') < 0.6) {
+				div.fadeTo('slow', 1);
+			}
+		});
+	}
+
+	function displayChart() {
+		if (myChart) {
+			myChart.stopTimer();
+			myChart.destroy();
+			myChart = null;
+		}
+		$("#widgetChart").show();
+
+		if (uptimeCapacitySettings.chartTypeId == "pie") {
+			myChart = new UPTIME.UptimeCapacityGadget({
+				dimensions : myChartDimensions,
+				chartDivId : "widgetChart",
+				chartType : uptimeCapacitySettings.chartTypeId,
+				elementId : uptimeCapacitySettings.elementId,
+				refreshInterval : uptimeCapacitySettings.refreshInterval
+			}, displayStatusBar, clearStatusBar);
+		} 
+		myChart.render();
+		$("body").height($(window).height());
+	}
+
+});
---
+++ src/uptimeCapacityGadget.js
@@ -0,0 +1,202 @@
+if (typeof UPTIME == "undefined") {
+	var UPTIME = {};
+}
+
+if (typeof UPTIME.UptimeCapacityGadget == "undefined") {
+	UPTIME.UptimeCapacityGadget = function(options, displayStatusBar, clearStatusBar) {
+		Highcharts.setOptions({
+			global : {
+				useUTC : false
+			}
+		});
+
+		var dimensions = new UPTIME.pub.gadgets.Dimensions(100, 100);
+		var chartDivId = null;
+		var elementId = null;
+		var chartType = null;
+		var refreshInterval = 30;
+		var chartTimer = null;
+		var api = new apiQueries();
+
+		var textStyle = {
+			fontFamily : "Verdana, Arial, Helvetica, sans-serif",
+			fontSize : "9px",
+			lineHeight : "11px",
+			color : "#565E6C"
+		};
+
+		if (typeof options == "object") {
+			dimensions = options.dimensions;
+			chartDivId = options.chartDivId;
+			chartType = options.chartType;
+			elementId = options.elementId;
+			refreshInterval = options.refreshInterval;
+		}
+
+		var seriesData = [ {
+			id : 'OK',
+			name : 'OK',
+			data : [ 0 ],
+			color : '#67B10B'
+		}, {
+			id : 'WARN',
+			name : 'WARN',
+			data : [ 0 ],
+			color : '#DAD60B'
+		}, {
+			id : 'CRIT',
+			name : 'CRIT',
+			data : [ 0 ],
+			color : '#B61211'
+		}, {
+			id : 'MAINT',
+			name : 'MAINT',
+			data : [ 0 ],
+			color : '#555B98'
+		}, {
+			id : 'UNKNOWN',
+			name : 'UNKNOWN',
+			data : [ 0 ],
+			color : '#AEAEAE'
+		} ];
+
+		var dataLabelsEnabled = false;
+		var chart = new Highcharts.Chart({
+			chart : {
+				renderTo : chartDivId,
+				width : dimensions.width,
+				height : dimensions.height,
+				type : chartType,
+				animation : true,
+				style : textStyle
+			},
+			credits : {
+				enabled : false
+			},
+			plotOptions : {
+				series : {
+					pointWidth : 20,
+					animation : true
+				}
+			},
+			title : {
+				text : '&nbsp;',
+				y : 5,
+				style : $.extend({
+					fontWeight : "bold"
+				}, textStyle),
+				useHTML : true
+			},
+			subtitle : {
+				text : '&nbsp;',
+				y : 20,
+				style : textStyle,
+				useHTML : true
+			},
+			xAxis : {
+				labels : {
+					enabled : false
+				}
+			},
+			yAxis : {
+				labels : {
+					style : textStyle,
+				},
+				allowDecimals : false,
+				min : 0,
+				title : {
+					text : ''
+				}
+			},
+			tooltip : {
+				style : textStyle,
+				formatter : function() {
+					if (dataLabelsEnabled) {
+						return '<b>' + this.series.name + '</b> - ' + monitorCount(this.y);
+					} else {
+						return '';
+					}
+				}
+			},
+			legend : {
+				enabled : false
+			},
+			series : seriesData,
+			spacingTop : 5,
+			spacingRight : 5,
+			spacingBottom : 5,
+			spacingLeft : 5
+		});
+
+		function requestData() {
+			api.getElementStatus(elementId).then(
+					function(result) {
+						if (!result.isMonitored) {
+							chart.hideLoading();
+							displayStatusBar("No visible elements to monitor", "Error Loading Chart Data");						
+							return;
+						}						
+						var statusCount = {
+							'OK' : 0,
+							'WARN' : 0,
+							'CRIT' : 0,
+							'UNKNOWN' : 0,
+							'MAINT' : 0
+						};
+						var total = 0;
+						$.each(result.monitorStatus, function(index, monitor) {
+							if (monitor.isMonitored && !monitor.isHidden) {
+								statusCount[monitor.status]++;
+								total++;
+							}
+						});
+						chart.setTitle({
+							text : '<a href="' + uptimeGadget.getElementUrls(result.id, result.name).services
+									+ '" target="_top">' + escapeHtml(result.name) + '</a>',
+						}, {
+							text : monitorCount(total),
+						});
+						$.each(statusCount, function(status, count) {
+							var bar = chart.get(status);
+							bar.setData([ count ]);
+							if (count > 0) {
+								bar.show();
+							} else {
+								bar.hide();
+							}
+						});
+						clearStatusBar();
+						dataLabelsEnabled = true;
+						chart.hideLoading();
+					}, function(error) {
+						chart.hideLoading();
+						displayStatusBar(error, "Error Loading Chart Data");
+					});
+			if (refreshInterval > 0) {
+				chartTimer = setTimeout(requestData, refreshInterval * 1000);
+			}
+		}
+
+		// public functions for this function/class
+		var publicFns = {
+			render : function() {
+				chart.showLoading();
+				requestData();
+			},
+			resize : function(dimensions) {
+				chart.setSize(dimensions.width, dimensions.height);
+			},
+			stopTimer : function() {
+				if (chartTimer) {
+					window.clearTimeout(chartTimer);
+				}
+			},
+			destroy : function() {
+				chart.destroy();
+			}
+		};
+		return publicFns; // Important: we need to return the public
+		// functions/methods
+
+	};
+}
